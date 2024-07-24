12bdb223ddc6b80cc276bc71b5d0a783f5edc58e
LachezarKunov <96826419+LachezarKunov@users.noreply.github.com>
2024-07-24 21:51:50 +0300

    Upload main.cpp

---
+++ Simple-Polymorphism/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+class Animal 
+{
+public:
+    virtual void makeSound() const = 0;
+
+    virtual ~Animal() {};
+};
+
+class Cat : public Animal 
+{
+public:
+    void makeSound() const override
+    {
+        cout << "Meow";
+    }
+};
+
+class Dog : public Animal 
+{
+public:
+    void makeSound() const override
+    {
+        cout << "Bark";
+    }
+};
+
+class Chicken : public Animal 
+{
+public:
+    void makeSound() const override 
+    {
+        cout << "Cluck";
+    }
+};
+
+
+int main()
+{
+    Animal** animals = new Animal*[3];
+
+    animals[0] = new Cat();
+    animals[1] = new Dog();
+    animals[2] = new Chicken();
+
+    for (size_t i = 0; i < 3; i++)
+    {
+        animals[i]->makeSound();
+        cout << " ";
+    }
+
+    for (size_t i = 0; i < 3; i++)
+    {
+        delete animals[i];
+    }
+
+    delete[] animals;
+}
